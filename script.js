4faac914fbd65734be03579b12dc47211a715785
289Volts <289volts@gmail.com>
2022-10-16 17:22:40 +0100

    tablet view done. desktop mode in progress

--- script.js
+++ script.js
@@ -4,23 +4,10 @@ const container = document.getElementsByClassName(".container");
 
 function toggle(e) 
 {
-    
     nav.classList.toggle('show');
-
-    if (nav.classList.contains('show')) {
-        open.src = "images/close-svgrepo-com.svg";
-
-    } else {
-        open.src = "images/menu-svgrepo-com.svg";
-    }
 }
 
 
 function hide() {
     nav.classList.remove("show");
-    if (nav.classList.contains("show")) {
-			open.src = "images/close-svgrepo-com.svg";
-		} else {
-			open.src = "images/menu-svgrepo-com.svg";
-		}
 }
